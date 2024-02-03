7f3b9cba49d4f9cb338bb9a82a99ddd1e52f9636
Ademar <ademar.gross@gmail.com>
2024-02-02 21:28:49 -0300

    Mais exercícios

--- 2606-categorias.sql
+++ 2606-categorias.sql
@@ -1,10 +1,13 @@
 /* 
+EXERCÍCIO 2606 - CATEGORIAS
+
+
 Quando os dados foram migrados de Banco de Dados, houve um pequeno mal-entendido por parte do antigo DBA.
 
 Seu chefe precisa que você exiba o código e o nome dos produtos, cuja categoria inicie com 'super'. 
 */
 
---MONTAGEM DAS TABELAS
+--MONTAGEM DAS TABELAS:
 CREATE TABLE categories (
 id numeric PRIMARY KEY,
 name varchar);
